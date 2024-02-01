d01657f84e0876f01d69f2d39df13980bce0da49
Adrian Alvarez Lopez <alvarez.lopez.adri@gmail.com>
2024-02-01 17:35:07 +0100

    Adding missed file

---
+++ utils/readrequirements/requirements_cfg.h
@@ -0,0 +1,20 @@
+/**
+ * @file requirements_cfg.h
+ * @brief Requirements customizable parameters
+ * @author Adrian Alvarez Lopez
+ * @version 1.0.0
+ * @date 2024-02-01
+ */
+
+#ifndef REQUIREMENTS_CONF_H
+#define REQUIREMENTS_CONF_H
+
+#define REQ_TITLE_LEN 128
+#define REQ_DESC_LEN  512
+#define REQ_MAX_LINKS  32
+
+#define MAX_REQUIREMENTS_REQUEST 10u
+
+#define MAX_N_REQ 1024
+
+#endif
